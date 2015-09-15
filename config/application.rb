b7115fb06d35372ee319707abf5268bf30d9e3a3
Grigoriy Dobryakov <grigoriy@dobryakov.com>
2015-09-15 17:45:06 +0300

    add devise for user

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Facegrid
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.web_console.whitelisted_ips = ENV['DEBUG_ALLOW_IP'] if Rails.env.development?
+
+    config.time_zone = 'UTC'
+
   end
 end
